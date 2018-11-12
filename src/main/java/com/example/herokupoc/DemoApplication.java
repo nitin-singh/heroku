3dccf47567eb13c7a0fe8356af7a75b527b5407f
Nitin Kumar Singh <nitin.kumar@tothenew.com>
2018-11-12 10:28:07 +0530

    First Commit. Created dummy project.

---
+++ src/main/java/com/example/herokupoc/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.herokupoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/")
+    	@ResponseBody
+    	String home() {
+      	return "Hello World!";
+    	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+}
